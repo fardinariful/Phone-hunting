0c1f9f4a60e8d172b215aac0d56b25bc0c41c1f5
fardinariful <fardinewucse2020@gmail.com>
2024-10-05 10:15:50 +0600

    something added

--- js/phn.js
+++ js/phn.js
@@ -22,7 +22,7 @@ phone=phone.slice(0,12);
 phon.textContent="";
 
 phone.forEach(ph =>{
-    console.log(ph);
+    //console.log(ph);
     
     const phonecard=document.createElement('div');
     phonecard.classList=`w-1/3 p-2`;
@@ -35,7 +35,7 @@ phone.forEach(ph =>{
                     <p class="font-bold">Name:"${ph.phone_name}"</p>
                     <p class="text-gray-600">There are many variations of passages of available, but the majority have suffered</p>
                     <p class="font-semibold">$999</p>
-                    <button onclick="show_details('${ph.slug}') ; show_phone_details.showModal()" class="bg-blue-500 text-white rounded py-2 px-4 mt-2 hover:bg-blue-600">Show Details</button>
+                    <button onclick="show_details('${ph.slug}') " class="bg-blue-500 text-white rounded py-2 px-4 mt-2 hover:bg-blue-600">Show Details</button>
                 </div>
     
     `;
@@ -46,12 +46,39 @@ toggleloading(false);
 }
 
 const show_details = async (id) => {
-console.log("click",id);
+//console.log("click",id);
 const res=await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
 const data= await res.json();
-console.log(data);
+const phone=data.data;
+showphonedetails(phone);
 
 
+
+}
+
+const showphonedetails=(phoneno)=>{
+    console.log(phoneno);
+    const phone_name=document.getElementById('show-details-phone-name');
+    phone_name.innerText=phoneno.name;
+
+    // const showdetailscontainer=document.getElementById("Show-details_container");
+    // showdetailscontainer.innerHTML=
+        //  <img src="${phoneno.image}" alt="">
+        //  <h3>Iphone 13 Pro Max</h3>
+        //  <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+        //  <p>Storage : 128GB/256GB/1TB Storage, No card slot</p>
+        //  <p>Display Size : 6.7 Inches, 109.8 cm</p>
+        //  <p>Chipset : Apple A15 Bionic</p>
+        //  <p>Memory : 128GB 6 GB RAM, 256GB RAM, 512GB RAM1 1TB 6GB RAM</p>
+        //  <p>Slug : Applice_Iphone_13_Pro_Max</p>
+        //  <p>Release data : Released 2021, September 24</p>
+        //  <p>Brand : Apple</p>
+        //  <p>GPS : AYes, with A-GPS, GLONASS, GALILEO, BDS, QZSS</p>
+
+    
+    
+   
+    show_phone_details.showModal();
 
 }
 const handler = () =>
@@ -72,5 +99,4 @@ const toggleloading = (isloading) =>{
     loadingbtn.classList.add('hidden');
 }
  }
- 
  
